0cc38929c9b989a72bca5fdb0062a5589f92460d
Hamid Machfudin Sukardi <44257661+hamidms@users.noreply.github.com>
2020-12-16 12:01:08 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Hamid's Vue.js App"/>
   </div>
 </template>
 
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 80px;
 }
 </style>
